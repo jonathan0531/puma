99ce90581ec5d419f516c8ba12f4a2f9f5235dfc
Jonathan <jthon@live.com>
2020-11-17 17:35:41 -0600

    api changes

--- client/src/services/LookbackService.js
+++ client/src/services/LookbackService.js
@@ -3,5 +3,8 @@ import Api from "@/services/Api";
 export default {
   getOneLookback(params) {
     return Api().get("lookback/" + params.id);
+  },
+  getNATLookback(params) {
+    return Api().get("nat/" + params.id);
   }
 };
